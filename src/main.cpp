47b39b9372aec2375d2db7886a70e1582fab7fca
vk2tds <darryl@radio-active.net.au>
2025-07-07 20:24:33 +1000

    Settings code is coming along rather nicely.

--- src/main.cpp
+++ src/main.cpp
@@ -1,182 +1,325 @@
-/*
- * Simple demo to demonstrate functionality of Ethernet and WiFi simultaneously 
- * If successful upon initializatoin on the terminal will be shown the IP adresses
- * of each connection. The output will be something like this:
- * ---------------------------------------------------------------------------------
- * Connecting to <SSID>
- * ETH Started
- * ....
- * WiFi connected
- * IP address: 
- * 192.168.0.204
- * ETH Connected
- * ETH MAC: 3C:71:BF:0D:89:5B, IPv4: 192.168.0.152, FULL_DUPLEX, 100Mbps
- * ---------------------------------------------------------------------------------
- * every 3 seconds:
- * 1) ethernet tries to connect to google.com
- * 2) WiFi tries to connect to data.sparkfun.com
- * For the WiFi you should edit the variables
- * "ssid" and "password" with the ones of your access point.
- */
+
+
+// To program...
+// Click House (Platform ID Build at the bottom)
+// Click Arror Right down the bottom.
+
+
+// Configuration sentences. NOTE: No sentences until received over serial port
+// GUID/settings/GUID = Set GUID from serial. 
+// GUID/settings/wifi = 1 or 0
+// GUID/settings/wifi/SSID = 
+// GUID/settings/wifi/password = 
+// GUID/settings/wifi/DHCP = 1 or 0
+// GUID/settings/wifi/IP = 192.168.0.123
+// GUID/settings/wifi/GW = 192.168.0.123
+// GUID/settings/wifi/mask = 255.255.255.0
+// GUID/settings/ether = 1 or 0
+// GUID/settings/ether/DHCP = 1 or 0
+// GUID/settings/ether/IP = 192.168.0.123
+// GUID/settings/ether/GW = 192.168.0.123
+// GUID/settings/ether/mask = 255.255.255.0
+// GUID/settings/mqtt/server = 192.168.0.73
+// GUID/settings/mqtt/port = 1833
+// GUID/settings/mqtt/SSL = 1 or 0
+// GUID/settings/mqtt/username = 
+// GUID/settings/mqtt/password =
+// GUID/settings/mqtt/maximumtime = 30 - send every 30 seconds even if no change
+// GUID/settings/mqtt/
+
+// settings to turn on fans even if no control?
+
+
+
+
+// void WiFiEvent(WiFiEvent_t event)
+// {
+//   switch (event) {
+//     case ARDUINO_EVENT_ETH_START:
+//       Serial.println("ETH Started");
+//       //set eth hostname here
+//       ETH.setHostname("esp32-ethernet");
+//       break;
+//     case ARDUINO_EVENT_ETH_CONNECTED:
+//       Serial.println("ETH Connected");
+//       break;
+//     case ARDUINO_EVENT_ETH_GOT_IP:
+//       Serial.print("ETH MAC: ");
+//       Serial.print(ETH.macAddress());
+//       Serial.print(", IPv4: ");
+//       Serial.print(ETH.localIP());
+//       if (ETH.fullDuplex()) {
+//         Serial.print(", FULL_DUPLEX");
+//       }
+//       Serial.print(", ");
+//       Serial.print(ETH.linkSpeed());
+//       Serial.println("Mbps");
+//       eth_connected = true;
+//       break;
+//     case ARDUINO_EVENT_ETH_DISCONNECTED:
+//       Serial.println("ETH Disconnected");
+//       eth_connected = false;
+//       break;
+//     case ARDUINO_EVENT_ETH_STOP:
+//       Serial.println("ETH Stopped");
+//       eth_connected = false;
+//       break;
+//     case SYSTEM_EVENT_ETH_START:
+//       Serial.println("SYS ETH Started");
+//       //set eth hostname here
+//       ETH.setHostname("esp32-ethernet");
+//       break;
+//     case SYSTEM_EVENT_ETH_CONNECTED:
+//       Serial.println("SYS ETH Connected");
+//       break;
+//     case SYSTEM_EVENT_ETH_GOT_IP:
+//       Serial.print("SYS ETH MAC: ");
+//       Serial.print(ETH.macAddress());
+//       Serial.print(", IPv4: ");
+//       Serial.print(ETH.localIP());
+//       if (ETH.fullDuplex()) {
+//         Serial.print(", FULL_DUPLEX");
+//       }
+//       Serial.print(", ");
+//       Serial.print(ETH.linkSpeed());
+//       Serial.println("Mbps");
+//       eth_connected = true;
+//       break;
+//     case SYSTEM_EVENT_ETH_DISCONNECTED:
+//       Serial.println("SYS ETH Disconnected");
+//       eth_connected = false;
+//       break;
+//     case SYSTEM_EVENT_ETH_STOP:
+//       Serial.println("SYS ETH Stopped");
+//       eth_connected = false;
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
+
+
+
+// // Callback function
+// void callback(char* topic, byte* payload, unsigned int length) {
+//   // In order to republish this payload, a copy must be made
+//   // as the orignal payload buffer will be overwritten whilst
+//   // constructing the PUBLISH packet.
+
+//   // Allocate the correct amount of memory for the payload copy
+//   byte* p = (byte*)malloc(length);
+//   // Copy the payload to the new buffer
+//   memcpy(p,payload,length);
+//   client.publish("outTopic", p, length);
+//   // Free the memory
+//   free(p);
+// }
+
+// https://github.com/espressif/arduino-esp32/issues/1262
 
 
 #include <ETH.h>
+#include <PubSubClient.h> // go to Sketch --> Include Library --> Manage Libraries... --> search for PubSubClient by Nick O'Leary
+#include <string.h>
 
-static bool eth_connected = false;
+void callback(char *topic_, byte *payload, unsigned int length);
 
-void WiFiEvent(WiFiEvent_t event)
-{
-  switch (event) {
-    case SYSTEM_EVENT_ETH_START:
-      Serial.println("ETH Started");
-      //set eth hostname here
-      ETH.setHostname("esp32-ethernet");
-      break;
-    case SYSTEM_EVENT_ETH_CONNECTED:
-      Serial.println("ETH Connected");
-      break;
-    case SYSTEM_EVENT_ETH_GOT_IP:
-      Serial.print("ETH MAC: ");
-      Serial.print(ETH.macAddress());
-      Serial.print(", IPv4: ");
-      Serial.print(ETH.localIP());
-      if (ETH.fullDuplex()) {
-        Serial.print(", FULL_DUPLEX");
-      }
-      Serial.print(", ");
-      Serial.print(ETH.linkSpeed());
-      Serial.println("Mbps");
-      eth_connected = true;
-      break;
-    case SYSTEM_EVENT_ETH_DISCONNECTED:
-      Serial.println("ETH Disconnected");
-      eth_connected = false;
-      break;
-    case SYSTEM_EVENT_ETH_STOP:
-      Serial.println("ETH Stopped");
-      eth_connected = false;
-      break;
-    default:
-      break;
-  }
-}
+#define BUTTON  34
 
-void testClient(const char * host, uint16_t port)
-{
-  Serial.print("\nconnecting to ");
-  Serial.println(host);
+struct settingsStruct {
+  boolean wifi_enable = false;
+  boolean wifi_dhcp = true;
+  IPAddress wifi_ip_address;
+  IPAddress wifi_ip_gateway;
+  IPAddress wifi_ip_mask;
+  IPAddress wifi_ip_DNS_1;
+  IPAddress wifi_ip_DNS_2;
+  char wifi_ssid[64];
+  char wifi_password[64];
 
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    return;
-  }
-  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
-  while (client.connected() && !client.available());
-  while (client.available()) {
-    Serial.write(client.read());
-  }
 
-  Serial.println("closing connection\n");
-  client.stop();
-}
+  boolean ether_enable = true;
+  boolean ether_dhcp = true;
+  IPAddress ether_ip_address;
+  IPAddress ether_ip_gateway;
+  IPAddress ether_ip_mask;
+  IPAddress ether_ip_DNS_1;
+  IPAddress ether_ip_DNS_2;
 
-#include <WiFi.h>
+  u16_t mqtt_port = 1833;
+  boolean mqtt_ssl = false;
+  u16_t mqtt_maximum_time = 30;
 
-const char* ssid     = "RUT_3DF7_2G";
-const char* password = "Zd32Pem7";
+  char mqtt_username [64];
+  char mqtt_password [64];
+  char mqtt_server [128];
+};
 
-const char* host = "data.sparkfun.com";
-const char* streamId   = "....................";
-const char* privateKey = "....................";
+struct settingsStruct mqttSettings;
 
-void setup()
-{
+// MQTT Broker
+const char *mqtt_broker = "2.0.1.1";
+const char *topic = "emqx/ESP32-POE_Button";
+const char *mqtt_username = "";
+const char *mqtt_password = "";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+    // Set software serial baud to 115200;
     Serial.begin(115200);
-    delay(10);
-
-    // ETH
-    Serial.begin(115200);
-    WiFi.onEvent(WiFiEvent);
     ETH.begin();
 
-    // We start by connecting to a WiFi network
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-}
-
-int value = 0;
-
-void loop()
-{
-  Serial.print("ETH MAC: ");
-  Serial.print(ETH.macAddress());
-  Serial.print(", IPv4: ");
-  Serial.println(ETH.localIP());
-
-  if (eth_connected) {
-    testClient("google.com", 80);
-  }
-    delay(3000);
-    ++value;
-
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort)) {
-        Serial.println("connection failed");
-        return;
-    }
-
-    // We now create a URI for the request
-    String url = "/input/";
-    url += streamId;
-    url += "?private_key=";
-    url += privateKey;
-    url += "&value=";
-    url += value;
-
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected()) {
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+            Serial.println("Public EMQX MQTT broker connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
         }
     }
+    // Publish and subscribe
+    client.publish(topic, "Hi, I'm Olimex board ESP32-POE\n\rThis is button example\n\rType \"SB\" to scan button level");
+    client.subscribe(topic);
 
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
+    pinMode (BUTTON, INPUT_PULLUP);
+}
+
+void callback(char *topic_, byte *payload, unsigned int length)
+{
+  char OutputMessage[32], SB=0;
+  payload[length]=0;
+
+  if (payload > 0){
+
+    if (strcmp(topic,"/settings/wifi/enable")==0){
+      if (payload[0] == '1'){
+        mqttSettings.wifi_enable = true;
+      }
+      if (payload[0] == '0'){
+        mqttSettings.wifi_enable = false;
+      }
+    }
+    if (strcmp(topic,"/settings/wifi/SSID")==0){
+      strncpy (mqttSettings.wifi_ssid, (char *)payload, 64);
+    }
+    if (strcmp(topic,"/settings/wifi/password")==0){
+      strncpy (mqttSettings.wifi_password, (char *)payload, 64);
+    }
+    if (strcmp(topic,"/settings/wifi/dhcp")==0){
+      if (payload[0] == '1'){
+        mqttSettings.wifi_dhcp = true;
+      }
+      if (payload[0] == '0'){
+        mqttSettings.wifi_dhcp = false;
+      }
+    }
+    if (strcmp(topic,"/settings/wifi/IP")==0){
+      mqttSettings.wifi_ip_address.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/wifi/GW")==0){
+      mqttSettings.wifi_ip_gateway.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/wifi/mask")==0){
+      mqttSettings.wifi_ip_mask.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/wifi/DNS_1")==0){
+      mqttSettings.wifi_ip_DNS_1.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/wifi/DNS_2")==0){
+      mqttSettings.wifi_ip_DNS_2.fromString((char*) payload);
     }
 
-    Serial.println();
-    Serial.println("closing connection");
-    delay(3000);
+
+
+    if (strcmp(topic,"/settings/ether/enable")==0){
+      if (payload[0] == '1'){
+        mqttSettings.ether_enable = true;
+      }
+      if (payload[0] == '0'){
+        mqttSettings.ether_enable = false;
+      }
+    }
+    if (strcmp(topic,"/settings/ether/dhcp")==0){
+      if (payload[0] == '1'){
+        mqttSettings.ether_dhcp = true;
+      }
+      if (payload[0] == '0'){
+        mqttSettings.ether_dhcp = false;
+      }
+    }
+    if (strcmp(topic,"/settings/ether/IP")==0){
+      mqttSettings.ether_ip_address.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/ether/GW")==0){
+      mqttSettings.ether_ip_gateway.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/ether/mask")==0){
+      mqttSettings.ether_ip_mask.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/wifi/DNS_1")==0){
+      mqttSettings.ether_ip_DNS_1.fromString((char*) payload);
+    }
+    if (strcmp(topic,"/settings/wifi/DNS_2")==0){
+      mqttSettings.ether_ip_DNS_2.fromString((char*) payload);
+    }
+
+
+    if (strcmp(topic,"/settings/mqtt/server")==0){
+      strncpy (mqttSettings.mqtt_server, (char *)payload, 64);
+    }
+    if (strcmp(topic,"/settings/mqtt/port")==0){
+      u16_t working = 0;
+      sscanf ((char *)payload, "%d", &working);
+      mqttSettings.mqtt_port = working; 
+
+    }
+    if (strcmp(topic,"/settings/mqtt/SSL")==0){
+      if (payload[0] == '1'){
+        mqttSettings.mqtt_ssl = true;
+      }
+      if (payload[0] == '0'){
+        mqttSettings.mqtt_ssl = false;
+      }
+    }
+    if (strcmp(topic,"/settings/mqtt/username")==0){
+      strncpy (mqttSettings.mqtt_username, (char *)payload, 64);
+    }
+    if (strcmp(topic,"/settings/mqtt/password")==0){
+      strncpy (mqttSettings.mqtt_password, (char *)payload, 64);
+    }
+
+    if (strcmp(topic,"/settings/mqtt/maximumtime")==0){
+      u16_t working = 0;
+      sscanf ((char *)payload, "%d", &working);
+      mqttSettings.mqtt_maximum_time = working; 
+    }
+  }
+
+
+
+
+
+
+
+  // SB  = strstr ((char*)payload, "SB")!=NULL;
+
+  // if (SB)
+  // {
+  //   sprintf (OutputMessage, "Button is %d\n\r", digitalRead(BUTTON));
+  //   client.publish((const char*)topic, (const char*)OutputMessage, strlen(OutputMessage));
+  // }
+}
+
+void loop() {
+    client.loop();
 }
